c2085870f9918103d3376e2f1487c13369d1539c
ahmed-debbech <debbech.ahmed@gmail.com>
2019-11-04 00:18:46 +0100

    Fix a glitch in snake mouvement

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main (int argc, char **argv){
   SDL_Event event;
   SDL_Event event_holder;
   while(game_done == false){
-    event = event_holder;
+    //event = event_holder;
     SDL_PollEvent(&event);
       switch(event.type){
         case SDL_QUIT: // to quit the game
